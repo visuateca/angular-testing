55d3abde91349a573d1321fa5d4f052e6510c839
Daniel Slivka <visuateca@gmail.com>
2021-09-21 13:38:30 +0200

    fakeAsync() test

--- unit-testing/components/component-with-async-service.ts
+++ unit-testing/components/component-with-async-service.ts
@@ -26,3 +26,16 @@ it('should show quote after component initialized', () => {
     expect(quoteEl.textContent).toBe(testQuote);
     expect(getQuoteSpy.calls.any()).toBe(true, 'getQuote() called');
 });
+
+// Async test with fakeAsync()
+// To use fakeAsync() functionality import zone.js/testing into test setup file (when not created by CLI)
+// The fakeAsync() function enables a linear coding style by running the test body in a special fakeAsync test zone
+// The fakeAsync() function won't work if the test body makes an XMLHttpRequest (XHR) call
+it('should display error when TwainService fails', fakeAsync(() => {
+    getQuoteSpy.and.returnValue(throwError('TwainService test failure'));
+    fixture.detectChanges(); // onInit()
+    tick(); // flush the component's setTimeout()
+    fixture.detectChanges();
+    expect(errorMessage()).toMatch(/test failure/, 'should display error');
+    expect(quoteEl.textContent).toBe('...', 'should show placeholder');
+}));
