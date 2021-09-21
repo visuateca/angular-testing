73890761cd036f1b19237ca93f9b089d98731200
Daniel Slivka <visuateca@gmail.com>
2021-09-21 09:34:43 +0200

    Setup of test

--- unit-testing/components/component-binding.ts
+++ unit-testing/components/component-binding.ts
@@ -1,1 +1,13 @@
 // Setup
+let component: BannerComponent;
+let fixture: ComponentFixture<BannerComponent>;
+let elementH1: HTMLElement;
+
+beforeEach(() => {
+    TestBed.configureTestingModule({
+        declarations: [BannerComponent]
+    });
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    elementH1 = fixture.nativeElement.querySelector('h1');
+});
