f822235c3cd8600d271302de72092745860135dd
Daniel Slivka <visuateca@gmail.com>
2021-09-21 09:54:30 +0200

    compileComonents() explicit call

--- unit-testing/components/component-with-external-files.ts
+++ unit-testing/components/component-with-external-files.ts
@@ -1,2 +1,4 @@
 // External templates and external styles
-
+// When run in CLI context external files are compiled
+// For non-CLI environments call compileComponents explicitly
+TestBed.compileComponents();
