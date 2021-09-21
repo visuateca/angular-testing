ea6f8b6aae131f9e41b4715c1997206fc7e2a72c
Daniel Slivka <visuateca@gmail.com>
2021-09-21 11:04:49 +0200

    compileComonents() explicit call

--- unit-testing/components/component-with-dependency.ts
+++ unit-testing/components/component-with-dependency.ts
@@ -1,1 +1,23 @@
 // Component with dependencies
+import { TestBed } from '@angular/core/testing';
+
+// Setup providers dependency in welcome.component.spec.ts
+TestBed.configureTestingModule({
+    declarations: [WelcomeComponent],
+    providers: [
+        { provide: UserService, useValue: userServiceStub } // use stub double, never real service
+    ]
+});
+
+// Provide service test doubles
+let userServiceStub: Partial<UserService>;
+userServiceStub = {
+    isLoggedIn: true,
+    user: { name: 'test_user' }
+}
+
+// Get injected services
+// The way that always works, is to get it from the injector of the component-under-test
+userService = fixture.debugElement.injector.get(UserService);
+// From root injector
+userService = TestBed.inject(UserService);
