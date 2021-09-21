9b6ff3900eba77cb8a4451e9478f64f137790d1e
Daniel Slivka <visuateca@gmail.com>
2021-09-21 12:05:54 +0200

    Setting up Spy on service by using createSpyObj()

--- unit-testing/components/component-with-async-service.ts
+++ unit-testing/components/component-with-async-service.ts
@@ -1,2 +1,20 @@
 // Working with asynchronous services
 
+// Only service's API matters. Always emulate calls to service
+// Testing with a Spy
+beforeEach(() => {
+    const testQuote = 'test';
+    const twainService = jasmine.createSpyObj('TwainService', ['getQuote']);
+    const getQuoteSpy = twainService.getQuote.and.returnValue(of(testQuote));
+
+    TestBed.configureTestingModule({
+        declarations: [TwainComponent],
+        providers: [
+            { provide: twainService, useValue: twainService }
+        ]
+    });
+});
+
+fixture = TestBed.createComponent(TwainComponent);
+comsponent = fixture.componentInstance;
+quoteEl = fixture.nativeElement.querySelector('.twain');
