b2c520eefd3c6ced799f87eb50818f5879abe499
Daniel Slivka <visuateca@gmail.com>
2021-09-22 07:10:08 +0200

    host -> child test

--- unit-testing/components/component-inside-test-host.ts
+++ unit-testing/components/component-inside-test-host.ts
@@ -1,1 +1,26 @@
+// Component inside a test host
 
+// The setup for the test-host tests is similar to the setup for the stand-alone tests
+// dashboard-hero.component.spec.ts
+TestBed.configureTestingModule({
+    declarations: [
+        DashboardHeroComponent,
+        TestHostComponent
+    ]
+});
+fixture = TestBed.createComponent(TestHostComponent);
+testHost = fixture.componentInstance;
+heroEl = fixture.nativeElement.querySelector('.hero');
+fixture.detectChanges(); // onInit
+
+// dashboard-hero.component.spec.ts
+it('should display hero name', () => {
+  const expectedPipedName = testHost.hero.name.toUpperCase();
+  expect(heroEl.textContent).toContain(expectedPipedName);
+});
+
+it('should raise selected event when clicked', () => {
+  click(heroEl);
+  // selected hero should be the same data bound hero
+  expect(testHost.selectedHero).toBe(testHost.hero);
+});
