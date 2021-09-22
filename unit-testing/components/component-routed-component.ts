d387f6e9d70b7783c6fc9eb06ef656f68a7d89d3
Daniel Slivka <visuateca@gmail.com>
2021-09-22 15:32:40 +0200

    Testing with ActivatedRouteStub

--- unit-testing/components/component-routed-component.ts
+++ unit-testing/components/component-routed-component.ts
@@ -29,4 +29,37 @@ export class ActivatedRouteStub {
 }
 
 // Testing with ActivatedRouteStub
+describe('when navigate to existing hero', () => {
+    let expectedHero: Hero;
 
+    beforeEach(async () => {
+        expectedHero = firstHero;
+        activatedRoute.setParamMap({ id: expectedHero.id });
+        await createComponent();
+    });
+});
+
+// Expects the component to try to navigate to the HeroListComponent
+describe('when navigate to non-existent hero id', () => {
+    beforeEach(async () => {
+        ActivatedRoute.setParamMap({ id: 9999 });
+        await createComponent();
+    })
+    it('should try to navigate back to hero list', () => {
+        expect(page.gotoListSpy.calls.any()).toBe(true, 'comp.gotoList called');
+        expect(page.navigateSpy.calls.any()).toBe(true, 'router.navigate called');
+    });
+});
+
+// Create and display a new hero
+describe('when navigate with no hero id', () => {
+    beforeEach(async () => {
+        await createComponent();
+    })
+    it('should have hero.id === 0', () => {
+        expect(component.hero.id).toBe(0);
+    });
+    it('should display empty hero name', () => {
+        expect(page.nameDisplay.textContent).toBe('');
+    });
+});
